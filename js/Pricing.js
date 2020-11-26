d46dd8a83d082b329601aaeb406d89e940c9cc44
Vishal643 <vishalkumar199812@gmail.com>
2020-11-26 11:22:55 +0530

    about us page is ready and made some changes in footer

--- js/Pricing.js
+++ js/Pricing.js
@@ -1,1 +1,13 @@
-var navTags = document.querySelector('a');
+var custombtn = document.getElementById("custombtn");
+function showContactUsPage (e) {
+    e.target.style.zIndex = "1";
+    window.location.replace("../html/contact-us.html");
+    console.log("Hello")
+    
+}
+custombtn.addEventListener('submit', showContactUsPage())
+
+
+
+
+
